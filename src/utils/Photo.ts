a641e4cf97912d33c8098fe4159aac57d1981a4e
yoshiharu152 <hikarinko3@icloud.com>
2026-09-05 15:41:23 +0900

    Update Photo.ts

--- src/utils/Photo.ts
+++ src/utils/Photo.ts
@@ -32,6 +32,44 @@ export function getPhotoGenderByTags(tags: string[]): PhotoGender {
     return PhotoGender.UNMARKED;
 }
 
+export const enum PhotoClothing {
+    ALL = 'all',
+    SUIT = 'suit',
+    SHIRT = 'shirt',
+    LOOSE = 'loose',
+    KIMONO = 'kimono',
+    INNER = 'inner'
+}
+
+export function getPhotoClothingByTags(tags: string[]): PhotoClothing {
+    const lowerTags = tags.map(t => t.toLowerCase());
+
+    // インナー・ピタッとした服・筋肉・身体のラインが分かるキーワード群
+    const innerKeywords = [
+        'inner', 'tight', 'swimsuit', 'swimwear', 'bikini', 'underwear', 
+        'bare', 'shirtless', 'nude', 'physique', 'muscle', 'fitness', 
+        'fit', 'athletic', 'tank top', 'sports bra', 'torso', 'bodybuilder', 'abs'
+    ];
+    if (lowerTags.some(t => innerKeywords.some(kw => t.includes(kw)))) {
+        return PhotoClothing.INNER;
+    }
+
+    if (lowerTags.some(t => ['suit', 'jacket', 'blazer', 'formal', 'tuxedo'].some(kw => t.includes(kw)))) {
+        return PhotoClothing.SUIT;
+    }
+    if (lowerTags.some(t => ['shirt', 'blouse', 'collared', 'dress-shirt'].some(kw => t.includes(kw)))) {
+        return PhotoClothing.SHIRT;
+    }
+    if (lowerTags.some(t => ['hoodie', 'sweatshirt', 'loose', 'oversized', 'coat', 'parka'].some(kw => t.includes(kw)))) {
+        return PhotoClothing.LOOSE;
+    }
+    if (lowerTags.some(t => ['kimono', 'yukata', 'traditional', 'haori', 'japanese'].some(kw => t.includes(kw)))) {
+        return PhotoClothing.KIMONO;
+    }
+
+    return PhotoClothing.ALL;
+}
+
 export default class Photo {
     id: string = '';
     width: number = 0;
@@ -44,6 +82,7 @@ export default class Photo {
     authorUsername: string = '';
 
     gender: PhotoGender = PhotoGender.UNMARKED;
+    clothing: PhotoClothing = PhotoClothing.ALL;
 
     /** @see https://google.github.io/mediapipe/solutions/pose.html */
     normalizedLandmarks: { point: [number, number, number], visibility: number }[] = [];
